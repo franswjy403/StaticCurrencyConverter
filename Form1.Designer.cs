a7cc617c3deb191e6cd22ee7b847e07cc92adad3
Frans Wijaya <franswijaya403@gmail.com>
2021-03-29 22:30:33 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.toBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(91, 100);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(237, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // toBox
+            // 
+            this.toBox.Location = new System.Drawing.Point(91, 184);
+            this.toBox.Name = "toBox";
+            this.toBox.ReadOnly = true;
+            this.toBox.Size = new System.Drawing.Size(237, 23);
+            this.toBox.TabIndex = 1;
+            this.toBox.TextChanged += new System.EventHandler(this.toBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "From:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "To:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "USD",
+            "IDR",
+            "EUR",
+            "RM",
+            "JPY"});
+            this.comboBox1.Location = new System.Drawing.Point(334, 100);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 23);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "USD",
+            "IDR",
+            "EUR",
+            "RM",
+            "JPY"});
+            this.comboBox2.Location = new System.Drawing.Point(334, 184);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(120, 23);
+            this.comboBox2.TabIndex = 5;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 300);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toBox);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox toBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        double input = 0.0, output = 0.0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if ((String)textBox1.Text!="")
+                input = Double.Parse(textBox1.Text);
+            else input = 0;
+            if ((String)comboBox1.SelectedItem == "USD")
+            {
+                if ((String)comboBox2.SelectedItem == "USD") output = input;
+                else if ((String)comboBox2.SelectedItem == "JPY") output = input * 109.71;
+                else if ((String)comboBox2.SelectedItem == "IDR") output = input * 14465.80;
+                else if ((String)comboBox2.SelectedItem == "EUR") output = input * 0.85;
+                else if ((String)comboBox2.SelectedItem == "RM") output = input * 4.15;
+                
+            }
+            else if ((String)comboBox1.SelectedItem == "JPY")
+            {
+                if ((String)comboBox2.SelectedItem == "USD") output = input * 0.0091;
+                else if ((String)comboBox2.SelectedItem == "JPY") output = input;
+                else if ((String)comboBox2.SelectedItem == "IDR") output = input * 131.69;
+                else if ((String)comboBox2.SelectedItem == "EUR") output = input * 0.0077;
+                else if ((String)comboBox2.SelectedItem == "RM") output = input * 0.038;
+            }
+            else if ((String)comboBox1.SelectedItem == "IDR")
+            {
+                if ((String)comboBox2.SelectedItem == "USD") output = input * 0.000069;
+                else if ((String)comboBox2.SelectedItem == "JPY") output = input * 0.0076;
+                else if ((String)comboBox2.SelectedItem == "IDR") output = input;
+                else if ((String)comboBox2.SelectedItem == "EUR") output = input * 0.000059;
+                else if ((String)comboBox2.SelectedItem == "RM") output = input * 0.00029;
+            }
+            else if ((String)comboBox1.SelectedItem == "EUR")
+            {
+                if ((String)comboBox2.SelectedItem == "USD") output = input * 1.18;
+                else if ((String)comboBox2.SelectedItem == "JPY") output = input * 129.13;
+                else if ((String)comboBox2.SelectedItem == "IDR") output = input * 17022.57;
+                else if ((String)comboBox2.SelectedItem == "EUR") output = input;
+                else if ((String)comboBox2.SelectedItem == "RM") output = input * 4.88;
+            }
+            else
+            {
+                if ((String)comboBox2.SelectedItem == "USD") output = input * 0.24;
+                else if ((String)comboBox2.SelectedItem == "JPY") output = input * 26.48;
+                else if ((String)comboBox2.SelectedItem == "IDR") output = input * 3489.29;
+                else if ((String)comboBox2.SelectedItem == "EUR") output = input * 0.20;
+                else if ((String)comboBox2.SelectedItem == "RM") output = input;
+            }
+            toBox.Text = output.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void toBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
